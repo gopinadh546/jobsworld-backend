f57c55628449a19ab1204888e0e80cbc97ee9833
T L SAI GOPINADH <gopinadh546@gmail.com>
2022-04-28 00:39:49 +0530

    final commit

---
+++ routes/jobs.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { authenticateToken } = require("../middleware/login");
+const router = express.Router();
+const {
+  getAllJobs,
+  getJob,
+  postJob,
+  renderEditForm,
+  deleteJob,
+  updateJob,
+  getAppliedApplicants,
+} = require("../controllers/jobs");
+
+router.route("/jobs").get(getAllJobs).post(authenticateToken, postJob);
+router.route("/jobs/:id").get(getJob).delete(deleteJob);
+router.route("/jobs/edit/:id").get(renderEditForm).put(updateJob);
+router.route("/jobs/:id/applicants").get(getAppliedApplicants);
+
+module.exports = router;
